9ecc1d615df19c195aa2ca3398f070797a7935d2
Stal Charles <stalcha01@gmail.com>
2023-08-30 15:22:34 +0200

    p*tin

--- FRONT/STATIQUE/Javascript/Javascript.js
+++ FRONT/STATIQUE/Javascript/Javascript.js
@@ -1,8 +1,11 @@
 var verification=document.getElementById('verification');
+var img=document.getElementById('img');
 var poutine=document.getElementById('putin');
 verification.addEventListener("mouseover" , (e) => {
     poutine.innerHTML='<audio src="Assets/son/balade.mp3" autoplay></audio>';
+    img.src="Assets/gif/wide-putin-awokenRes.gif";
 });
 verification.addEventListener("mouseout" , (e) => {
     poutine.innerHTML='';
+    img.src="Assets/food/resized/Food-Name-433.png";
 });
