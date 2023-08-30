d8a9c1ee8f5c69587385bd2eef6ba70478927369
Stal Charles <stalcha01@gmail.com>
2023-08-30 15:04:32 +0200

    easter egg

---
+++ FRONT/STATIQUE/Javascript/Javascript.js
@@ -0,0 +1,10 @@
+var verification=document.getElementById('verification');
+var poutine=document.getElementById('putin');
+verification.addEventListener("mouseover" , (e) => {
+    poutine.innerHTML='<audio src="Assets/son/balade.mp3" autoplay></audio>';
+});
+verification.addEventListener("mouseout" , (e) => {
+    poutine.innerHTML='';
+});
+
+window.alert('ok');
