6947285a671b969861c0d4e64cd56c7a6afe0cc4
Stal Charles <stalcha01@gmail.com>
2023-08-31 11:53:46 +0200

    catégorie_burger

--- FRONT/STATIQUE/Javascript/Javascript.js
+++ FRONT/STATIQUE/Javascript/Javascript.js
@@ -2,7 +2,7 @@ var verification=document.getElementById('verification');
 var img=document.getElementById('img');
 var poutine=document.getElementById('putin');
 verification.addEventListener("mouseover" , (e) => {
-    poutine.innerHTML='<audio src="Assets/son/balade.mp3" autoplay></audio>';
+    poutine.innerHTML='<audio src="Assets/son/balade.mp3" autoplay loop></audio>';
     img.src="Assets/gif/wide-putin-awokenRes.gif";
 });
 verification.addEventListener("mouseout" , (e) => {
