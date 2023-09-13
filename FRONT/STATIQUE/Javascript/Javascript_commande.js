b5bf2cb87ca87c5f83f03ddb278d149a612671af
Stal Charles <stalcha01@gmail.com>
2023-09-13 16:48:44 +0200

    formu adresse presque perfecto

--- FRONT/STATIQUE/Javascript/Javascript_commande.js
+++ FRONT/STATIQUE/Javascript/Javascript_commande.js
@@ -1,12 +1,14 @@
 var filtre = new RegExp("^[A-Za-z]+$");
 var filtrenp=new RegExp("^[A-Za-z]+ +[A-Za-z]+$");
-var filtretel = new RegExp("^[0-9]{10}$");
+var filtretel = new RegExp("^0+[0-9]{9}$");
+var filtreadresse= new RegExp("^[0-9]+ +[A-Za-zéèàêëîôû]+$");
 var filtremail= new RegExp("^[A-Za-z0-9.-]+@[A-Za-z0-9.-]{2,}.[A-Za-z]{2,4}$");
 let form=document.getElementById('form1');
 let defaultname=document.getElementById('defaultnp');
 let defaultmail=document.getElementById('defaultmail');
 let defaultnumero=document.getElementById('defaultnumero');
 let defaultadresse=document.getElementById('defaultadresse');
+let adressepourverif,chainea,chaineb,chaineba,chainebb;
 
 
 form.addEventListener('submit' , (e) => {
@@ -14,6 +16,22 @@ form.addEventListener('submit' , (e) => {
     defaultmail.innerHTML="";
     defaultnumero.innerHTML="";
     defaultadresse.innerHTML="";
+    adressepourverif=form.elements['adresse'].value;
+    chainea=adressepourverif.substring(0,(adressepourverif.indexOf(' ')+1));
+    console.log(chainea);
+    chaineb=adressepourverif.substring(adressepourverif.indexOf(' ')+1,adressepourverif.length);
+    console.log(chaineb);
+    while (chaineb.indexOf(' ')!=-1)
+    {
+     chaineba=chaineb.substring(0,chaineb.indexOf(' '));
+     console.log(chaineba);
+     chainebb=chaineb.substring(chaineb.indexOf(' ')+1,chaineb.length);
+     console.log(chainebb);
+     chaineb=chaineba+chainebb;
+     console.log(chaineb);
+    }
+    adressepourverif=chainea+chaineb;
+    console.log(adressepourverif);
 
     if (form.elements['np'].value ==='' || form.elements['np']== null)
     {
@@ -39,6 +57,11 @@ form.addEventListener('submit' , (e) => {
     {
         defaultadresse.innerHTML = "* veuillez renseigner l'adresse  <br>";
         e.preventDefault();
+    }else if(filtreadresse.test(adressepourverif)==false)
+    {
+     defaultadresse.innerHTML = "* Veuillez saisir une adresse valide. <br>";
+     e.preventDefault();
+     console.log(form.elements['adresse'].value);
     }
     if (form.elements['telephone'].value=='')
     {
