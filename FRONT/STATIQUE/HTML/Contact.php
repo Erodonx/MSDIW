d83c7edd4cc65100040540aa656d0d021b1d1c16
Stal Charles <stalcha01@gmail.com>
2023-10-12 11:05:03 +0200

    POINT A LA LIGNE!

--- FRONT/STATIQUE/HTML/Contact.php
+++ FRONT/STATIQUE/HTML/Contact.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 
 $datecourante = date("Y-m-d-H-i-s");
 $fp=fopen("$datecourante.'.txt'","x+");
-fputs($fp,"Nom : ".$_REQUEST['nom'] . " Prenom : " . $_REQUEST['prenom'] . " Email : " .$_REQUEST['mail'] . " Téléphone : ".$_REQUEST['telephone'] . " Demande : " .$_REQUEST['demande']);
+fputs($fp,"Nom : ".$_REQUEST['nom'] . "\nPrenom : " . $_REQUEST['prenom'] . "\nEmail : " .$_REQUEST['mail'] . "\nTéléphone : ".$_REQUEST['telephone'] . "\nDemande : " .$_REQUEST['demande']);
 
 
 
