1914ded98e3b2a79d38668ed13e5b0fd7c69a500
Stal Charles <stalcha01@gmail.com>
2023-10-12 16:00:31 +0200

    nav nav nav

--- FRONT/PHP_exos/fonctions/index.php
+++ FRONT/PHP_exos/fonctions/index.php
@@ -7,10 +7,10 @@ function RepertoireCourant ()
     $cheminrelatif=substr($cheminabsolu,$pos).'/';
     return $cheminrelatif;
 }
-echo '<a href="/">Retour au menu</a><br><br>';
+echo '<a href="/FRONT/PHP_exos/index.php">Retour au menu</a><br><br>';
 foreach (glob("*.*") as $f1) {
     if(($f1!= '.') && ($f1!= '..') && ($f1 != '.htaccess') && ($f1!= 'index.php'))
-         echo '<a href="'.RepertoireCourant().$f1.'">'.$f1.'</a> ' .date("d-m-Y H:i", filemtime($f1)). '</br>';
+         echo '<a href="/FRONT/PHP_exos'.RepertoireCourant().$f1.'">'.$f1.'</a> ' .date("d-m-Y H:i", filemtime($f1)). '</br>';
 }
 
 ?>
